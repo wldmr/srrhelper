5726e0b121e8bad77a05b651ae0a7571a815351f
Andreas Waldenburger <andreas@waldenburger.name>
2015-05-23 00:59:09 +0200

    Moved file reading/parsing to CPack.java .

--- src/net/wldmr/srrcpack/CPack.java
+++ src/net/wldmr/srrcpack/CPack.java
@@ -1,40 +1,62 @@
 package net.wldmr.srrcpack;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CPack {
+
+	private static enum RE {
+		group_start ("^\\s*(\\w+) \\{$"),
+		group_end ("^\\s*\\}$"),
+		property_string ("^\\s*(\\w+): \"([^\"]*)\"$"),
+		property_value ("^\\s*(\\w+): (\\w+)$");
+
+		private final Pattern pattern;
+		RE(String regex) {
+			pattern = Pattern.compile(regex);
+		}
+
+		Matcher match(String input) {
+			return pattern.matcher(input);
+		}
+	}
+
 	public static abstract class Node {
-		Branches parent;
+		Tree parent;
 		String key;
-		
-		public Node(Branches parent, String key) {
+
+		public Node(Tree parent, String key) {
 			super();
 			this.parent = parent;
 			this.key = key;
 		}
 	}
-	
+
 	public static class Leaf extends Node {
 		String value;
 
-		public Leaf(Branches parent, String key, String value) {
+		public Leaf(Tree parent, String key, String value) {
 			super(parent, key);
 			this.value = value;
 		}
 
 	}
-	
-	public static class Branches extends Node {
+
+	public static class Tree extends Node {
 		Map<String, List<Node>> nodes;
 
-		public Branches(Branches parent, String key) {
+		public Tree(Tree parent, String key) {
 			super(parent, key);
 			this.nodes = new HashMap<String, List<Node>>();
 		}
-		
+
 		public void add(Node node) {
 			if (nodes.containsKey(node.key)) {
 				nodes.get(node.key).add(node);
@@ -42,6 +64,53 @@ public class CPack {
 				List<Node> newList = new ArrayList<>();
 				newList.add(node);
 				nodes.put(node.key, newList);
+			}
+		}
+	}
+
+
+	public static void build(String filename) {
+		// Open the file
+
+		String strLine;
+		
+		//Read File Line By Line
+		try (
+				FileReader fr = new FileReader(filename);
+				BufferedReader br =	new BufferedReader(fr);
+			) {
+			while ((strLine = br.readLine()) != null) {
+				handleLine(strLine);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return;
+		}
+	}
+
+	private static void handleLine(String strLine) {
+		for (RE re : RE.values()) {
+			Matcher m = re.match(strLine);
+			if (m.matches()) {
+				switch (re.name()) {
+				case "group_start":
+					System.out.println("start: " + m.group(1));
+					break;
+				case "group_end":
+					System.out.println("end");
+					break;
+				case "property_string":
+					System.out.println("property (string): "
+							+ m.group(1) + " = " + m.group(2));
+					break;
+				case "property_value":
+					System.out.println("property (value): "
+							+ m.group(1) + " = " + m.group(2));
+					break;
+				default:
+					System.out.println("WUT?!");
+				}
 			}
 		}
 	}
--- src/net/wldmr/srrcpack/Main.java
+++ src/net/wldmr/srrcpack/Main.java
@@ -1,65 +1,13 @@
 package net.wldmr.srrcpack;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.regex.*;
 
 public class Main {
 	
-	private static enum RE {
-		group_start ("^\\s*(\\w+) \\{$"),
-		group_end ("^\\s*\\}$"),
-		property_string ("^\\s*(\\w+): \"([^\"]*)\"$"),
-		property_value ("^\\s*(\\w+): (\\w+)$");
-		
-		private final Pattern pattern;
-		RE(String regex) {
-			pattern = Pattern.compile(regex);
-		}
-		
-		Matcher match(String input) {
-			return pattern.matcher(input);
-		}
-	}
-
-	public static void main(String[] args) throws Exception {
-		// Open the file
-		FileInputStream fstream = new FileInputStream("UnderGroundClub.srt.txt");
-		BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
-
-		String strLine;
-
-		//Read File Line By Line
-		while ((strLine = br.readLine()) != null) {
-			for (RE re : RE.values()) {
-				Matcher m = re.match(strLine);
-				if (m.matches()) {
-					switch (re.name()) {
-					case "group_start":
-						System.out.println("start: " + m.group(1));
-						break;
-					case "group_end":
-						System.out.println("end");
-						break;
-					case "property_string":
-						System.out.println("property (string): "
-								+ m.group(1) + " = " + m.group(2));
-						break;
-					case "property_value":
-						System.out.println("property (value): "
-								+ m.group(1) + " = " + m.group(2));
-						break;
-					default:
-						throw new Exception("Fuck!");
-					}
-				}
-			}
-		}
-
-		//Close the input stream
-		br.close();
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		System.out.println(args.length);
+		CPack.build(args[0]);
 	}
 
 }
